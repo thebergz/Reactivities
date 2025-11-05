2fc7331d33fa295a5a7d32a80d48f0654856e035
Andre Bergmann <thebergz@gmail.com>
2025-11-05 19:24:54 +1100

    chore: end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
