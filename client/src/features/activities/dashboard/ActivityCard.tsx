2ca98dfcf0e73dc2d16da7c1c031c4b8d8f0d90d
Andre Bergmann <thebergz@gmail.com>
2025-11-10 21:41:13 +1100

    chore: end of section 7

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,11 +1,11 @@
 import {Box, Button, Card, CardActions, CardContent, Chip, Typography} from "@mui/material";
 import {useActivities} from "../../../lib/hooks/useActivities.ts";
+import {Link} from "react-router";
 
 type Props = {
     activity: Activity
-    selectActivity: (id: string) => void;
 }
-export default function ActivityCard({activity, selectActivity}: Props) {
+export default function ActivityCard({activity}: Props) {
     const {deleteActivity} = useActivities();
 
     return (
@@ -19,7 +19,7 @@ export default function ActivityCard({activity, selectActivity}: Props) {
             <CardActions sx={{display: 'flex', justifyContent: 'space-between', pb: 2}}>
                 <Chip label={activity.category} variant="outlined" />
                 <Box display="flex" gap={3}>
-                    <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
+                    <Button component={Link} to={`/activities/${activity.id}`} size="medium" variant="contained">View</Button>
                     <Button
                         onClick={() => deleteActivity.mutateAsync(activity.id)}
                         disabled={deleteActivity.isPending}
