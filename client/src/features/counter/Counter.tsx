46e8a2ae96840001ffe1926a2554ff416d5f7af1
Andre Bergmann <thebergz@gmail.com>
2025-11-11 21:24:22 +1100

    chore: end of section 9

---
+++ client/src/features/counter/Counter.tsx
@@ -0,0 +1,31 @@
+import {useStore} from "../../lib/hooks/useStore.ts";
+import {Box, Button, ButtonGroup, List, ListItem, Paper, Typography} from "@mui/material";
+import {observer} from "mobx-react-lite";
+
+const Counter= observer(function Counter() {
+    const {counterStore} = useStore();
+
+    return (
+        <Box display="flex" justifyContent="space-between">
+            <Box sx={{width: '60%'}}>
+                <Typography variant="h4" gutterBottom>{counterStore.title}</Typography>
+                <Typography variant="h6">The count is: {counterStore.count}</Typography>
+                <ButtonGroup sx={{mt: 3}}>
+                    <Button onClick={() => counterStore.decrement()} variant="contained" color="error">Decrement</Button>
+                    <Button onClick={() => counterStore.increment()} variant="contained" color="success">Increment</Button>
+                    <Button onClick={() => counterStore.increment(5)} variant="contained" color="primary">Increment by 5</Button>
+                </ButtonGroup>
+            </Box>
+            <Paper sx={{width: '40%', p: 4}}>
+                <Typography variant="h5">Counter events {counterStore.eventCount}</Typography>
+                <List>
+                    {counterStore.events.map((event, index) => (
+                        <ListItem key={index}>{event}</ListItem>
+                        ))}
+                </List>
+            </Paper>
+        </Box>
+    )
+});
+
+export default Counter;
--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,12 +1,20 @@
 import axios from 'axios'
+import {store} from "../stores/store.ts";
 
 const sleep = (delay: number) => {
-    return new Promise(resolve => setTimeout(resolve, delay))
+    return new Promise(resolve => {
+        setTimeout(resolve, delay)
+    });
 }
 
 const agent = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
+
+agent.interceptors.request.use(config => {
+    store.uiStore.isBusy()
+    return config;
+})
 
 agent.interceptors.response.use(async response => {
     try {
@@ -15,6 +23,8 @@ agent.interceptors.response.use(async response => {
     } catch (error) {
         console.log(error);
         return Promise.reject(error)
+    } finally {
+        store.uiStore.isIdle()
     }
 })
 
