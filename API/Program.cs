8624323fedc54bdff43db09210225ee7e6fdf172
Andre Bergmann <thebergz@gmail.com>
2025-11-26 20:32:04 +1100

    chore: end of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -3,14 +3,22 @@ using Application.Activities.DTOs;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
+using Domain;
 using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -24,6 +32,12 @@ builder.Services.AddMediatR(x =>
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+    {
+        opt.User.RequireUniqueEmail = true; //Important because the SignInManager API passes email not userId - email/userId are somewhat ineterchangeable but IdentityFramework only forces unique UserID (not email) by default
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
@@ -31,8 +45,17 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
+app.UseAuthentication(); //Authentication MUST COME BEFORE authorization
+app.UseAuthorization();
+
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>(); // api/login
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -40,8 +63,9 @@ var services = scope.ServiceProvider;
 try
 {
    var context = services.GetRequiredService<AppDbContext>();
+   var userManager = services.GetRequiredService<UserManager<User>>();
    await context.Database.MigrateAsync();
-   await DbInitializer.SeedData(context);
+   await DbInitializer.SeedData(context, userManager);
 }
 catch (Exception ex)
 {
